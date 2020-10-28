4e6f88352007895ee4bf7249fd983fae37bc3daf
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2020-10-28 10:12:13 +0100

    2nd version: some minor changes

--- get_entropy.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def get_entropy(bundle, password):
-    return len(password) * math.log2(len(bundle))
--- password_generator.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def password_generator(bundle, length=12):
-    random.shuffle(bundle)
-    password = random.choices(bundle, k=length)
-    password = ''.join(password)
-    return password
